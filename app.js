ae11b313d9a26c6895da10a923672051c0987de5
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-10-14 20:17:25 +0600

    Create Routes

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
+
+const contactRoutes = require("./contactRoutes");
 
 const app = express();
+app.use(morgan("dev"));
+
+app.use("/contacts", contactRoutes);
+
+app.get("*", (req, res) => {
+  res.send(<h1>Please use the Correct Routes</h1>);
+});
 
 const PORT = process.env.PORT || 8080;
 
